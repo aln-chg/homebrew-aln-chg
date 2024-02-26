d09873b121bc3482e529e7bf65fa82b3e5d4989f
aln-chg <shroudrowdy0203@gmail.com>
2024-02-25 18:03:13 -0600

    new url

--- Formula/storage-cli.rb
+++ Formula/storage-cli.rb
@@ -1,7 +1,7 @@
 class StorageCli < Formula
   desc "A CLI tool that lets you see the storage info of your computer"
   homepage "https://github.com/aln-chg/storage-cli/tree/main"
-  url "https://github.com/aln-chg/storage-cli/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/aln-chg/storage-cli/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "cc4e6d16dfc8c0af913ce9618a42807123e547958e92e1a5cdbcce2ffedabc96"
   license "MIT"
 
